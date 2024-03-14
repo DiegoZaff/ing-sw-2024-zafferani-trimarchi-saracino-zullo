dba6155ab104bb91f4fe41c63243b6e0d35dd558
ZulloFrancesco <francesco1.zullo@mail.polimi.it>
2024-03-14 18:26:16 +0100

    Inserito funzionamento di playInitialCard e playCard. Aggiunto removeCard, cambiato hand da private a public

--- src/main/java/it/polimi/ingsw/gc28/model/Player.java
+++ src/main/java/it/polimi/ingsw/gc28/model/Player.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.gc28.model;
 
+import it.polimi.ingsw.gc28.model.cards.CardGame;
 import it.polimi.ingsw.gc28.model.cards.CardResource;
 import it.polimi.ingsw.gc28.model.objectives.Objective;
 
@@ -10,13 +11,30 @@ public class Player {
 
     private int points;
 
+    /*
+    *  rimuove la carta passata come parametro dalla mano
+    *
+    *
+    * */
+    public void removeCard (ArrayList<CardResource> hand, CardGame cardToBeRemoved) {
+        for (CardResource card : hand) {
+            if (card == cardToBeRemoved) // le carte devono essere cnonfrontate, avranno un codice identificativo? equals? ==?
+            {
+                hand.remove(card);
+            }
+
+        }
+
+    }
+
+
     public int getPoints(){
         return points;
     }
 
     private Objective objective;
 
-    private ArrayList<CardResource> hand;
+    public ArrayList<CardResource> hand; //reso pubblico per far funzionare removecard non so se si può
 
     private Table table;
 }
