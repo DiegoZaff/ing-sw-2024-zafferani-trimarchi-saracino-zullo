3f51c79cdaaa21a3c3537ebf214d8b12b17e8196
DiegoZaff <diego.zaffe@gmail.com>
2024-04-11 14:40:01 +0200

    getPlayerFromNick

--- src/test/java/it/polimi/ingsw/gc28/games/assertions/GameAssertion.java
+++ src/test/java/it/polimi/ingsw/gc28/games/assertions/GameAssertion.java
@@ -1,10 +1,17 @@
 package it.polimi.ingsw.gc28.games.assertions;
 
 import it.polimi.ingsw.gc28.model.Game;
+import it.polimi.ingsw.gc28.model.Player;
+
+import java.util.Optional;
 
 public abstract class GameAssertion {
 
     public abstract boolean verifyAssertion(Game game);
 
     public abstract String toString();
+
+    public Optional<Player> getPlayerFromNick(String playerNick, Game game){
+        return game.getPlayers().stream().filter(p -> p.getName().equals(playerNick)).findFirst();
+    }
 }
