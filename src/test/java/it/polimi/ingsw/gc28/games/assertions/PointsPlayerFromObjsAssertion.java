01fd04e5317f35fbb2d6e3ed77080c7259b04f4b
DiegoZaff <diego.zaffe@gmail.com>
2024-04-11 14:50:14 +0200

    PointsPlayerFromObjsAssertion

---
+++ src/test/java/it/polimi/ingsw/gc28/games/assertions/PointsPlayerFromObjsAssertion.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.gc28.games.assertions;
+
+import it.polimi.ingsw.gc28.model.Game;
+import it.polimi.ingsw.gc28.model.Player;
+
+import java.util.Optional;
+
+public class PointsPlayerFromObjsAssertion  extends GameAssertion{
+
+    private final String playerNick;
+
+    private final int points;
+
+    private Integer actualPoints;
+
+    public PointsPlayerFromObjsAssertion(String playerNick, int points) {
+        this.playerNick = playerNick;
+        this.points = points;
+    }
+
+
+    @Override
+    public boolean verifyAssertion(Game game) {
+        Optional<Player> player =  super.getPlayerFromNick(playerNick, game);
+
+        if(player.isEmpty()){
+            actualPoints = null;
+            return false;
+        }
+
+        actualPoints = player.get().getObjectivePoints();
+
+        return points == actualPoints;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("PointsPlayerFromObjsAssertion Assertion --- expectedPoints: %s, actual: %s", points, actualPoints);
+    }
+}
