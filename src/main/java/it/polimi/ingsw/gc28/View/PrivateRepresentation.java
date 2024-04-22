3f76555fb5264abb32ee91ddb73004705d024b76
Giacomo Saracino <giacomo.saracino@mail.polimi.it>
2024-04-22 16:19:50 +0200

    Created GameRepresentation for the View

---
+++ src/main/java/it/polimi/ingsw/gc28/View/PrivateRepresentation.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.gc28.View;
+
+import it.polimi.ingsw.gc28.model.Table;
+import it.polimi.ingsw.gc28.model.cards.CardObjective;
+import it.polimi.ingsw.gc28.model.cards.CardResource;
+
+import java.util.ArrayList;
+
+public class PrivateRepresentation {
+
+    private ArrayList<CardResource> hand;
+
+    private CardObjective privateObjective;
+
+    private Table table;
+
+}
