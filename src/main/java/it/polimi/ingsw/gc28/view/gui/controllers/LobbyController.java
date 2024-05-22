7dc4d52bc323478ecb86a21265b018be9f13f559
AlessandroTrimarchi <alessandro.trimarchi@mail.polimi.it>
2024-05-22 17:13:55 +0200

    gui test

--- src/main/java/it/polimi/ingsw/gc28/view/gui/controllers/LobbyController.java
+++ src/main/java/it/polimi/ingsw/gc28/view/gui/controllers/LobbyController.java
@@ -1,8 +1,10 @@
 package it.polimi.ingsw.gc28.view.gui.controllers;
 
 import it.polimi.ingsw.gc28.view.gui.GuiApplication;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -13,7 +15,9 @@ import java.util.ResourceBundle;
 public class LobbyController implements Initializable {
     @FXML
     public TextField usernameTextField;
+    public TextField numberOfPlayersTextField;
     public ImageView backgroundImageView;
+    public Button createButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -24,5 +28,10 @@ public class LobbyController implements Initializable {
         GuiApplication.connection.closeConnection();
         GuiApplication.connection = null;
         GuiApplication.setRootPage("menu");
+    }
+
+    public void handleCreateButton(MouseEvent mouseEvent) {
+        //TODO: controllare
+        Platform.runLater(() -> GuiApplication.setRootPage("createGame"));
     }
 }
