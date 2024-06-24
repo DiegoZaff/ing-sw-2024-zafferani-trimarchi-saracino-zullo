35b379cb93aeaa0cc6d91b73309f167ab815533e
AlessandroTrimarchi <alessandro.trimarchi@mail.polimi.it>
2024-06-24 15:44:35 +0200

    random useName if texField is Empty

--- src/main/java/it/polimi/ingsw/gc28/network/messages/client/MsgJoinGame.java
+++ src/main/java/it/polimi/ingsw/gc28/network/messages/client/MsgJoinGame.java
@@ -5,6 +5,8 @@ import it.polimi.ingsw.gc28.model.errors.types.FailedActionManaged;
 import it.polimi.ingsw.gc28.network.rmi.VirtualView;
 
 import java.rmi.RemoteException;
+import java.util.Random;
+
 /**
  * message sent from the client to the server to join a game
  */
@@ -28,7 +30,9 @@ public class MsgJoinGame extends MessageC2S{
     public MsgJoinGame(String gameId, String userName) {
         super(MessageTypeC2S.JOIN_GAME ,gameId);
         this.userName = userName;
-
+        if(this.userName.isEmpty()){
+            this.userName = generateRandomString(4);
+        }
     }
 
     @Override
@@ -43,5 +47,18 @@ public class MsgJoinGame extends MessageC2S{
     @Override
     public void attachVirtualView(VirtualView client){
         setClient(client);
+    }
+
+    public static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            sb.append(characters.charAt(index));
+        }
+
+        return sb.toString();
     }
 }
